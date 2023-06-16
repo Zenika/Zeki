b30555dd9b95588e62d3fcf60809cb2d93352a2a
Kevin Lacavalerie <kevin.lacavalerie@zenika.com>
2023-06-16 22:54:13 +0200

    wip

---
+++ zeki-front/src/app/services/profile/profile-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+interface Profile {
+  url: string;
+  name: string;
+  surname: string;
+  location: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileServiceService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  profilesUrl = 'http://localhost:8080/images/all'
+
+  getProfiles(): Observable<Profile[]>{
+    return this.http.get<Profile[]>(this.profilesUrl);
+  }
+  
+}
