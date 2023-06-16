9e32afbfef1760f4949ef608db25c0935e357cd2
Kevin Lacavalerie <kevin.lacavalerie@zenika.com>
2023-06-16 22:54:13 +0200

    wip

--- zeki-front/src/app/app.component.ts
+++ zeki-front/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ProfileService } from './services/profile/profile.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'zeki-front';
+
+  constructor(private profileService: ProfileService) {
+  }
+
+  ngOnInit() {
+    this.profileService.getProfile().subscribe( (data) => {
+        console.log(data);
+    })
+  }
+
 }
