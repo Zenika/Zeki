13b1acd52446c9d0c43d6f5b07c4e55d4dc5a52a
Kevin Lacavalerie <kevin.lacavalerie@zenika.com>
2023-06-17 03:24:21 +0200

    dirty af

--- zeki-back/routes/images.js
+++ zeki-back/routes/images.js
@@ -35,29 +35,75 @@ router.get('/random', function (req, res, next) {
     res.json(imageUrls);
 });
 
-router.get('/all', function (req, res, next) {
+function getPicturesFiles() {
     const imagePath = path.join(__dirname, '../public/images');
-    const imageFiles = fs.readdirSync(imagePath);
-    // Envoi de l'image
-    const imageUrls = imageFiles.map((fileName) => {
-        const parsedFile = fileName.split("_");
-        const name = parsedFile[0];
-        const surname = parsedFile[1];
-        const location = parsedFile[2].split(".")[0];
+    return fs.readdirSync(imagePath);
+}
+
+function getProfilesFromPictures(picturesFiles) {
+    return picturesFiles.map((fileName) => {
+        const splitFileName = fileName.split("_");
+        const name = splitFileName[0];
+        const surname = splitFileName[1];
+        const location = splitFileName[2].split(".")[0];
         return {
-            url: `${req.protocol}://${req.get('host')}/images/${fileName}`,
             name: name,
             surname: surname,
             location: location
         }
     });
-    res.json(imageUrls);
+}
+
+router.get('/all', function (req, res, next) {
+    const picturesFiles = getPicturesFiles();
+    const profiles = getProfilesFromPictures(picturesFiles);
+    const profilesWithUrl = profiles.map((profile) => {
+        return {
+            ...profile,
+            url: `${req.protocol}://${req.get('host')}/images/${profile.name}_${profile.surname}_${profile.location}.jpg`
+        }
+    })
+    res.json(profilesWithUrl);
 });
 
-function randomNotIn(length, alreadyExist) {
-    let random;
-    while (!alreadyExist.includes(random = Math.floor(Math.random() * length))) {
+router.get('/questions', function (req, res, next) {
+    const picturesFiles = getPicturesFiles();
+    const profiles = getProfilesFromPictures(picturesFiles);
+    let questions = profiles.map((profile) => {
+        const actualProfile = {name: profile.name, surname: profile.surname }
+        const fakeProfile = getFakeProfile()
+        const randomProfile1 =  getRandomProfile()
+        const randomProfile2 = getRandomProfile()
+        return {
+            picture: `${req.protocol}://${req.get('host')}/images/${profile.name}_${profile.surname}_${profile.location}.jpg`,
+            solution: actualProfile,
+            answers: shuffle([
+                actualProfile,
+                fakeProfile,
+                randomProfile1,
+                randomProfile2
+            ])
+        }
+    })
+    res.json(questions);
+});
 
+
+function shuffle(array) {
+    let shuffledArray = [];
+    while (array.length > 0) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        shuffledArray.push(array[randomIndex]);
+        array.splice(randomIndex, 1);
+    }
+    return shuffledArray;
+}
+
+
+function randomNotIn(length, alreadyExist) {
+    let random = Math.floor(Math.random() * length);
+    while (alreadyExist.includes(random)) {
+        random = Math.floor(Math.random() * length);
     }
     return random;
 }
@@ -73,5 +119,104 @@ function parseFile(randomImage) {
         location
     };
 }
+
+function getRandomProfile() {
+    const picturesFiles = getPicturesFiles();
+    const profiles = getProfilesFromPictures(picturesFiles);
+    const randomIndex = Math.floor(Math.random() * profiles.length);
+    const randomProfile = profiles[randomIndex];
+    return {
+        name: randomProfile.name,
+        surname: randomProfile.surname
+    }
+}
+
+function getFakeProfile() {
+    const randomIndex = Math.floor(Math.random() * fakeProfile.length);
+    return fakeProfile[randomIndex];
+}
+
+const fakeProfile = [
+    {
+        "name": "Thomas",
+        "surname": "Pesquet"
+    },
+    {
+        "name": "Omar",
+        "surname": "Sy"
+    },
+    {
+        "name": "Kylian",
+        "surname": "Mbappé"
+    },
+    {
+        "name": "Kim",
+        "surname": "Kardashian"
+    },
+    {
+        "name": "Bob",
+        "surname": "Léponge"
+    },
+    {
+        "name": "Elon",
+        "surname": "Musk"
+    },
+    {
+        "name": "Maitre",
+        "surname": "Gims"
+    },
+    {
+        "name": "Pierre",
+        "surname": "Niney"
+    },
+    {
+        "name": "Jonathan",
+        "surname": "Cohen"
+    },
+    {
+        "name": "Tahar",
+        "surname": "Rahim"
+    },
+    {
+        "name": "Leïle",
+        "surname": "Bekhti"
+    },
+    {
+        "name": "Izïa",
+        "surname": "Higelin"
+    },
+    {
+        "name": "Carl",
+        "surname": "Pitt"
+    },
+    {
+        "name": "Romain",
+        "surname": "Depp"
+    },
+    {
+        "name": "Patrick",
+        "surname": "Swayze"
+    },
+    {
+        "name": "Rocky",
+        "surname": "Balboa"
+    },
+    {
+        "name": "Marion",
+        "surname": "Cotillard"
+    },
+    {
+        "name": "Eva",
+        "surname": "Green"
+    },
+    {
+        "name": "Nathalie",
+        "surname": "Porteman"
+    },
+    {
+        "name": "Tonton",
+        "surname": "David"
+    }
+]
 
 module.exports = router;
